5052150db88dbc247d69ac8d984f4f467fb44a1d
NINGMEI <NINGMEI@NINGMEI-Q>
2022-10-29 15:53:19 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace HeadPhoto999
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "001 (1).jpg");
+            this.imageList1.Images.SetKeyName(1, "001 (2).jpg");
+            this.imageList1.Images.SetKeyName(2, "001 (3).jpg");
+            this.imageList1.Images.SetKeyName(3, "001 (4).jpg");
+            this.imageList1.Images.SetKeyName(4, "001 (5).jpg");
+            this.imageList1.Images.SetKeyName(5, "001 (6).jpg");
+            this.imageList1.Images.SetKeyName(6, "001 (7).jpg");
+            this.imageList1.Images.SetKeyName(7, "001 (8).jpg");
+            this.imageList1.Images.SetKeyName(8, "001 (9).jpg");
+            this.imageList1.Images.SetKeyName(9, "001 (10).jpg");
+            this.imageList1.Images.SetKeyName(10, "001 (11).jpg");
+            this.imageList1.Images.SetKeyName(11, "001 (12).jpg");
+            this.imageList1.Images.SetKeyName(12, "001 (13).jpg");
+            this.imageList1.Images.SetKeyName(13, "001 (14).jpg");
+            this.imageList1.Images.SetKeyName(14, "001 (15).jpg");
+            this.imageList1.Images.SetKeyName(15, "001 (16).jpg");
+            this.imageList1.Images.SetKeyName(16, "001 (17).jpg");
+            this.imageList1.Images.SetKeyName(17, "001 (18).jpg");
+            this.imageList1.Images.SetKeyName(18, "001 (19).jpg");
+            this.imageList1.Images.SetKeyName(19, "001 (20).jpg");
+            this.imageList1.Images.SetKeyName(20, "001 (21).jpg");
+            this.imageList1.Images.SetKeyName(21, "001 (22).jpg");
+            this.imageList1.Images.SetKeyName(22, "001 (23).jpg");
+            this.imageList1.Images.SetKeyName(23, "001 (24).jpg");
+            this.imageList1.Images.SetKeyName(24, "001 (25).jpg");
+            this.imageList1.Images.SetKeyName(25, "001 (26).jpg");
+            this.imageList1.Images.SetKeyName(26, "001 (27).jpg");
+            this.imageList1.Images.SetKeyName(27, "001 (28).jpg");
+            this.imageList1.Images.SetKeyName(28, "001 (29).jpg");
+            this.imageList1.Images.SetKeyName(29, "001 (30).jpg");
+            this.imageList1.Images.SetKeyName(30, "001 (31).jpg");
+            this.imageList1.Images.SetKeyName(31, "001 (32).jpg");
+            this.imageList1.Images.SetKeyName(32, "001 (33).jpg");
+            this.imageList1.Images.SetKeyName(33, "001 (34).jpg");
+            this.imageList1.Images.SetKeyName(34, "001 (35).jpg");
+            this.imageList1.Images.SetKeyName(35, "001 (36).jpg");
+            this.imageList1.Images.SetKeyName(36, "001 (37).jpg");
+            this.imageList1.Images.SetKeyName(37, "001 (38).jpg");
+            this.imageList1.Images.SetKeyName(38, "001 (39).jpg");
+            this.imageList1.Images.SetKeyName(39, "001 (40).jpg");
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "form1";
+            this.Text = "透明窗体";
+            this.TransparencyKey = System.Drawing.SystemColors.Control;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeadPhoto999
+{
+    public partial class form1 : Form
+    {
+        public form1()
+        {
+            InitializeComponent();
+
+            //必须把绘图函数写到Paint的响应函数里，不然图像绘制出来会一闪而过
+            this.Paint += new PaintEventHandler(DrawHeadPhotos);
+        }
+        
+
+        private void DrawHeadPhotos(object sender,PaintEventArgs e)
+        {
+            //创建新的线程，以达到非阻塞效果，不然程序没法退出，键盘鼠标事件写了也没用
+            Thread thread = new Thread(DrawAsync);
+            thread.Start();
+        }
+
+        private void DrawAsync()
+        {
+            Screen screen = Screen.PrimaryScreen;
+            int width = screen.WorkingArea.Width;
+            int height = screen.WorkingArea.Height;
+            Random random = new Random();
+
+            Graphics graphics = this.CreateGraphics();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                int x = random.Next(0, width);
+                int y = random.Next(0, height);
+                Point[] points = { new Point(x, y), new Point(x + 50, y), new Point(x, y + 50) };
+                int index = random.Next(0, imageList1.Images.Count);
+                Image img = imageList1.Images[index];
+                graphics.DrawImage(img, points);
+
+                Thread.Sleep(100);
+
+            }
+            graphics.Dispose();
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            /*switch (keyData)
+            {
+                case Keys.F1:
+                    //Stuff
+                    return true;
+                case Keys.Alt | Keys.Down:
+                    //Stuff
+                    return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);*/
+
+            //this.Close();//这个只关闭窗体，程序并未退出
+            //Application.Exit();//这个程序虽然会退出，但是会等待Msg响应完成之后才退出，显然不行
+            System.Environment.Exit(0);//这个才是彻底退出
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
